d6caeb078e33ac08c5fba54add9952c05852ba5d
SirMaverick <rickruiten@hotmail.com>
2018-01-19 01:02:17 +0100

    Spacecraft_Stage

---
+++ Assets/Scripts/SpaceShipController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipController : MonoBehaviour {
+
+    CameraController camController;
+    [SerializeField] GameObject spaceShip;
+
+    float axisHor;
+    float axisVer;
+
+	// Use this for initialization
+	void Start () {
+        camController = GetComponent<CameraController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        axisHor = Input.GetAxis("Horizontal") * 3.0f * Time.deltaTime;
+        axisVer = Input.GetAxis("Vertical") * 3.0f * Time.deltaTime;
+
+        Vector3 inputVector = axisHor * transform.right + axisVer * transform.forward;
+        Vector3 worldInputVector = spaceShip.transform.InverseTransformDirection(inputVector);
+        spaceShip.transform.position += worldInputVector;
+
+        /*if (axisHor != 0 && axisVer != 0) {
+            Vector3 aimPoint = camController.transform.forward * axisVer + camController.transform.right * axisHor;
+            print(aimPoint);
+            spaceShip.transform.LookAt(aimPoint);
+        }*/
+        /*
+        Vector3 cameraToWorld = camController.transform.TransformDirection(camController.transform.forward);
+        print(cameraToWorld);
+        Vector3 normCameraToWorld = cameraToWorld.normalized;
+
+        spaceShip.transform.position += new Vector3(spaceShip.transform.right.x * normCameraToWorld.x * axisHor, 0, spaceShip.transform.forward.z * normCameraToWorld.z * axisVer);
+        */
+        /*
+        float angle = camController.transform.eulerAngles.y;
+        Vector3 lookPoint = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+        print(lookPoint);
+        //spaceShip.transform.position += new Vector3(spaceShip.transform.InverseTransformDirection(lookPoint).x * 3.0f * Time.deltaTime, 0, spaceShip.transform.InverseTransformDirection(lookPoint).z * 3.0f * Time.deltaTime);
+        //print(spaceShip.transform.InverseTransformDirection(lookPoint));
+        spaceShip.transform.position += spaceShip.transform.InverseTransformDirection(axisHor * lookPoint * transform.right.x + axisVer * (lookPoint + transform.forward));
+        //print(lookPoint);
+        //print((axisHor * lookPoint));
+        */
+        //spaceShip.transform.position += new Vector3(lookPoint.x * axisHor, 0, lookPoint.z * axisVer);
+
+
+
+        /*
+         * gebruikteVector ( input x , 0 , input z ) naar World
+         * object dat verplaatst wordt + gebruikteVectorWorld
+          
+         
+
+
+    */
+    }
+}
---
+++ Assets/Scripts/SpaceShipMovement.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipMovement : MonoBehaviour {
+
+    RaycastHit hit;
+    Vector3 frontPos, midPos, backPos, leftPos, rightPos;
+    public float frontHeight, midHeight;
+    [SerializeField] float minDistance;
+
+    string heightDifference;
+
+    public float yRotation = 0.0F;
+    public float xRotation = 0.0f;
+
+    public float xPosition;
+    public float zPosition;
+
+    // Use this for initialization
+    void Start () {
+	}
+
+    // Update is called once per frame
+    void Update() {
+        //xPosition = Input.GetAxis("Horizontal") * 3.0f * Time.deltaTime;
+        //zPosition = Input.GetAxis("Vertical") * 3.0f * Time.deltaTime;  
+        //transform.localPosition += transform.forward * xPosition;
+        //transform.eulerAngles = new Vector3(xRotation, yRotation, 0);
+
+        midPos = transform.position;
+        frontPos = midPos + transform.forward;
+        backPos = midPos - transform.forward;
+
+        if (Physics.Raycast(midPos, -transform.up, out hit)) {
+            midHeight = hit.distance;
+        }
+        if (Physics.Raycast(frontPos, -transform.up, out hit)) {
+            frontHeight = hit.distance;
+        }
+
+        if (midHeight - frontHeight > 0.02) {
+            RotateSpaceShip(1.0f);
+        } else if (midHeight - frontHeight < -0.02) {
+            RotateSpaceShip(-1.0f);
+        }
+
+        if(midHeight < minDistance - 0.02f) {
+            MoveSpaceShip(1);
+        } else if (midHeight > minDistance + 0.02f) {
+            MoveSpaceShip(-1);
+        }
+
+    }    
+        /*Physics.Raycast(transform.position, Vector3.down, out hit);
+        if(hit.collider) {
+            height = hit.distance;
+            if(height < minDistance) {
+                MoveStarshipVertical();
+            }
+        }
+
+        Physics.Raycast(transform.position, transform.forward, out hit);
+        if(hit.collider) {
+            horizontalDistance = hit.distance;
+            degrees = Mathf.Atan(horizontalDistance / height) * Mathf.Rad2Deg;
+            print(degrees);
+            if (horizontalDistance < minDistance ) {
+                
+            }
+            if(degrees > 10.0f) {
+                RotateSpaceShip();
+            }
+        }*/
+
+
+    void RotateSpaceShip(float a) {
+        xRotation -= 90.0f * a * Time.deltaTime;
+        transform.eulerAngles = new Vector3(xRotation, yRotation, 0);
+    }
+
+    void MoveSpaceShip(float a) {
+        Vector3 position = transform.position;
+        position.y += 5.0f * a * Time.deltaTime;
+        transform.position = position;
+    }
+}
